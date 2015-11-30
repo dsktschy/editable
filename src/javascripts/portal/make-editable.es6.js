c02bc06256bc01fef95b4b60df72a7c0e2fb46f5
dai <chamestica@gmail.com>
2015-12-01 03:49:13 +0900

    Get editable.min.js from RawGit

--- src/javascripts/portal/make-editable.es6.js
+++ src/javascripts/portal/make-editable.es6.js
@@ -2,7 +2,7 @@ export default () => {
   var className, jquerySrc, editableSrc, appendScript;
   className = 'editable-script';
   jquerySrc = '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js';
-  editableSrc = '//raw.githubusercontent.com/dsktschy/editable/gh-pages/javascripts/editable.min.js';
+  editableSrc = '//rawgit.com/dsktschy/editable/gh-pages/javascripts/editable.min.js';
   appendScript = (src, onload) => {
     var elem;
     elem = document.createElement('script');
