9b44a0aacf9cdd967ccaa58174264319d1e4a668
dai <chamestica@gmail.com>
2018-09-18 03:00:49 +0900

    Enable to download HTML file to click

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
-import 'sanitize.css'
-import 'reset-css'
 import './main.css'
 
-console.log('editable v2.0.0')
+const messageNotSupported = 'This browser is not supported.'
+const readyToDownloadHtml = true
+const fileNameDefault = 'index.html'
+const elTriggerDownload = elementize(`
+<div data-editable="trigger-download"></div>
+`)
+
+function elementize (str = '') {
+  const el = document.createElement('div')
+  el.innerHTML = str
+  return el.firstElementChild
+}
+function downloadHtml () {
+  if (!readyToDownloadHtml) return
+  const fileName = location.pathname.split('/').pop() || fileNameDefault
+  const blob = new Blob([ document.documentElement.outerHTML ])
+  if (navigator.msSaveBlob) {
+    navigator.msSaveBlob(blob, fileName)
+    return
+  }
+  const el = document.createElement('a')
+  el.setAttribute('download', fileName)
+  el.setAttribute('href', URL.createObjectURL(blob))
+  el.dispatchEvent(new MouseEvent('click'))
+}
+
+// Check supported or not
+if (FileReader == null) throw new Error(messageNotSupported)
+// Trigger to download
+elTriggerDownload.addEventListener('click', downloadHtml)
+document.body.insertBefore(elTriggerDownload, document.body.firstChild)
