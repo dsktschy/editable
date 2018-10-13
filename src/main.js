e468fd6abaec4f0cef6136b2a95a13a35ea18445
dai <chamestica@gmail.com>
2018-10-13 18:09:57 +0900

    Don't use window and window.document in classes

--- src/main.js
+++ src/main.js
@@ -1,99 +1,5 @@
 import './main.css'
 
-class App {
-  static get messageNotSupported () {
-    return 'This browser is not supported.'
-  }
-  static get fileNameDefault () {
-    return 'index.html'
-  }
-  constructor () {
-    this.appHtml = new AppHtml({
-      onClickAppTriggerDownload: this.downloadHtml.bind(this)
-    })
-    this.onSupportedBrowser = FileReader != null
-    // Output p instead of div on Enter
-    document.execCommand('defaultParagraphSeparator', false, 'p')
-  }
-  downloadHtml () {
-    const el = document.createElement('a')
-    const fileName = location.pathname.split('/').pop() || App.fileNameDefault
-    this.deactivate()
-    const html = this.appHtml.el.outerHTML
-    this.activate()
-    const doctype = new XMLSerializer().serializeToString(document.doctype)
-    const blob = new Blob([ doctype + html ])
-    el.setAttribute('download', fileName)
-    el.setAttribute('href', URL.createObjectURL(blob))
-    el.dispatchEvent(new MouseEvent('click'))
-  }
-  activate () {
-    this.appHtml.activate()
-  }
-  deactivate () {
-    this.appHtml.deactivate()
-  }
-}
-class AppHtml {
-  constructor ({ onClickAppTriggerDownload }) {
-    this.el = document.documentElement
-    this.appBody = new AppBody({ onClickAppTriggerDownload })
-    this.setActivatedMark()
-  }
-  setActivatedMark () {
-    this.el.setAttribute('data-editable', '')
-  }
-  removeActivatedMark () {
-    this.el.removeAttribute('data-editable')
-  }
-  activate () {
-    this.setActivatedMark()
-    this.appBody.activate()
-  }
-  deactivate () {
-    this.removeActivatedMark()
-    this.appBody.deactivate()
-  }
-}
-class AppBody {
-  constructor ({ onClickAppTriggerDownload }) {
-    this.el = document.body
-    this.appTriggerDownload = new AppTriggerDownload({
-      onClick: onClickAppTriggerDownload
-    })
-    this.appTargets = Array.from(document.querySelectorAll(AppTarget.selector))
-      .map(el => new AppTarget({ el }))
-    this.appUnitParents = this.createAppUnitParents()
-    this.appendTrigger()
-  }
-  createAppUnitParents () {
-    const appUnitElements = Array.from(document.querySelectorAll(AppUnit.selector))
-    const appUnitParentElements = Array.from(new Set(appUnitElements.map(el => el.parentNode)))
-    const map = new Map()
-    for (let el of appUnitParentElements) map.set(el, [])
-    for (let el of appUnitElements) map.get(el.parentNode).push(el)
-    return appUnitParentElements.map(el => new AppUnitParent({
-      el,
-      appUnitElements: map.get(el)
-    }))
-  }
-  appendTrigger () {
-    this.el.appendChild(this.appTriggerDownload.el)
-  }
-  removeTrigger () {
-    this.el.removeChild(this.appTriggerDownload.el)
-  }
-  activate () {
-    this.appendTrigger()
-    for (let appTarget of this.appTargets) appTarget.activate()
-    for (let appUnitParent of this.appUnitParents) appUnitParent.activate()
-  }
-  deactivate () {
-    this.removeTrigger()
-    for (let appTarget of this.appTargets) appTarget.deactivate()
-    for (let appUnitParent of this.appUnitParents) appUnitParent.deactivate()
-  }
-}
 class AppTriggerDownload {
   static get template () {
     return '<div data-editable="trigger-download"></div>'
@@ -107,36 +13,32 @@ class AppTarget {
   static get selector () {
     return '[data-editable="target"]'
   }
-  constructor ({ el }) {
+  constructor ({ el, onKeydown }) {
     this.el = el
-    // Wrap first line with p
-    this.el.addEventListener('keydown', this.doIfEmpty.bind(this, paragraphize))
-    this.enableToEdit()
+    this.onKeydown = this.doIfEmpty.bind(this, onKeydown)
   }
   doIfEmpty (fn) {
     if (!this.el.innerHTML) fn()
   }
-  enableToEdit () {
+  activate () {
+    this.el.addEventListener('keydown', this.onKeydown)
     this.el.setAttribute('contenteditable', '')
   }
-  disableToEdit () {
-    this.el.removeAttribute('contenteditable')
-  }
-  activate () {
-    this.enableToEdit()
-  }
   deactivate () {
-    this.disableToEdit()
+    this.el.removeAttribute('contenteditable')
+    this.el.removeEventListener('keydown', this.onKeydown)
   }
 }
 class AppUnitParent {
-  constructor ({ el, appUnitElements }) {
+  constructor ({ el, appUnitElements, createAppTargets }) {
     this.el = el
+    this.createAppTargets = createAppTargets
     this.appUnits = appUnitElements.map(el => this.createAppUnit({ el }))
   }
   createAppUnit ({ el }) {
     return new AppUnit({
       el,
+      createAppTargets: this.createAppTargets,
       onClickAppTriggerInsertUnitCloneBefore: this.insertAppUnitCloneBefore.bind(this),
       onClickAppTriggerInsertUnitCloneAfter: this.insertAppUnitCloneAfter.bind(this),
       onClickAppTriggerRemoveUnit: this.removeAppUnit.bind(this)
@@ -144,17 +46,19 @@ class AppUnitParent {
   }
   insertAppUnitCloneBefore (appUnit) {
     const appUnitClone = this.createAppUnit({ el: appUnit.createElementClone() })
-    this.appUnits.push(appUnitClone)
     this.el.insertBefore(appUnitClone.el, appUnit.el)
+    appUnitClone.activate()
+    this.appUnits.push(appUnitClone)
   }
   insertAppUnitCloneAfter (appUnit) {
     const appUnitClone = this.createAppUnit({ el: appUnit.createElementClone() })
-    this.appUnits.push(appUnitClone)
     this.el.insertBefore(appUnitClone.el, appUnit.el.nextSibling)
+    appUnitClone.activate()
+    this.appUnits.push(appUnitClone)
   }
   removeAppUnit (appUnit) {
-    this.el.removeChild(appUnit.el)
     this.appUnits.splice(this.appUnits.indexOf(appUnit), 1)
+    this.el.removeChild(appUnit.el)
     appUnit = null
   }
   activate () {
@@ -170,11 +74,15 @@ class AppUnit {
   }
   constructor ({
     el,
+    createAppTargets,
     onClickAppTriggerInsertUnitCloneBefore,
     onClickAppTriggerInsertUnitCloneAfter,
     onClickAppTriggerRemoveUnit
   }) {
     this.el = el
+    this.appTargets = createAppTargets({
+      appTargetElements: [ ...this.el.querySelectorAll(AppTarget.selector) ]
+    })
     this.appTriggerInsertUnitCloneBefore = new AppTriggerInsertUnitCloneBefore({
       onClick: () => { onClickAppTriggerInsertUnitCloneBefore(this) }
     })
@@ -185,8 +93,6 @@ class AppUnit {
       onClick: () => { onClickAppTriggerRemoveUnit(this) }
     })
     this.editedStyle = false
-    this.setPositionRelative()
-    this.appendTriggerElements()
   }
   setPositionRelative () {
     if (getComputedStyle(this.el, null).position !== 'static') return
@@ -203,29 +109,25 @@ class AppUnit {
       this.el.removeAttribute('style')
     }
   }
-  appendTriggerElements () {
-    this.el.appendChild(this.appTriggerInsertUnitCloneBefore.el)
-    this.el.appendChild(this.appTriggerInsertUnitCloneAfter.el)
-    this.el.appendChild(this.appTriggerRemoveUnit.el)
-  }
-  removeTriggerElements () {
-    this.el.removeChild(this.appTriggerInsertUnitCloneBefore.el)
-    this.el.removeChild(this.appTriggerInsertUnitCloneAfter.el)
-    this.el.removeChild(this.appTriggerRemoveUnit.el)
-  }
   createElementClone () {
-    this.removeTriggerElements()
+    this.deactivate()
     const el = this.el.cloneNode(true)
-    this.appendTriggerElements()
+    this.activate()
     return el
   }
   activate () {
     this.setPositionRelative()
-    this.appendTriggerElements()
+    this.el.appendChild(this.appTriggerInsertUnitCloneBefore.el)
+    this.el.appendChild(this.appTriggerInsertUnitCloneAfter.el)
+    this.el.appendChild(this.appTriggerRemoveUnit.el)
+    for (let appTarget of this.appTargets) appTarget.activate()
   }
   deactivate () {
+    for (let appTarget of this.appTargets) appTarget.deactivate()
+    this.el.removeChild(this.appTriggerInsertUnitCloneBefore.el)
+    this.el.removeChild(this.appTriggerInsertUnitCloneAfter.el)
+    this.el.removeChild(this.appTriggerRemoveUnit.el)
     this.removePositionRelative()
-    this.removeTriggerElements()
   }
 }
 class AppTriggerInsertUnitCloneBefore {
@@ -256,9 +158,62 @@ class AppTriggerRemoveUnit {
   }
 }
 
-const app = new App()
-if (!app.onSupportedBrowser) throw new Error(App.messageNotSupported)
+const messageNotSupported = 'This browser is not supported.'
+const fileNameDefault = 'index.html'
+const app = {}
 
+if (FileReader == null) alert(messageNotSupported)
+else {
+  app.triggerDownload = new AppTriggerDownload({ onClick: download })
+  app.unitParents = createAppUnitParents()
+  const appTargetElements = [ ...document.body.querySelectorAll(AppTarget.selector) ]
+    .filter(el => el.closest(AppUnit.selector) == null)
+  app.targets = createAppTargets({ appTargetElements })
+  activate()
+}
+
+function download () {
+  const el = document.createElement('a')
+  const fileName = location.pathname.split('/').pop() || fileNameDefault
+  deactivate()
+  const html = document.documentElement.outerHTML
+  activate()
+  const doctype = new XMLSerializer().serializeToString(document.doctype)
+  const blob = new Blob([ doctype + html ])
+  el.setAttribute('download', fileName)
+  el.setAttribute('href', URL.createObjectURL(blob))
+  el.dispatchEvent(new MouseEvent('click'))
+}
+function createAppTargets ({ appTargetElements }) {
+  return appTargetElements.map(el => new AppTarget({ el, onKeydown: paragraphize }))
+}
+function createAppUnitParents () {
+  const appUnitElements = [ ...document.body.querySelectorAll(AppUnit.selector) ]
+  const appUnitParentElements = [ ...new Set(appUnitElements.map(el => el.parentNode)) ]
+  const map = new Map()
+  for (let el of appUnitParentElements) map.set(el, [])
+  for (let el of appUnitElements) map.get(el.parentNode).push(el)
+  return appUnitParentElements.map(el => new AppUnitParent({
+    el,
+    appUnitElements: map.get(el),
+    createAppTargets
+  }))
+}
+function activate () {
+  // Output p instead of div on Enter
+  document.execCommand('defaultParagraphSeparator', false, 'p')
+  document.documentElement.setAttribute('data-editable', '')
+  document.body.appendChild(app.triggerDownload.el)
+  for (let appTarget of app.targets) appTarget.activate()
+  for (let appUnitParent of app.unitParents) appUnitParent.activate()
+}
+function deactivate () {
+  for (let appUnitParent of app.unitParents) appUnitParent.deactivate()
+  for (let appTarget of app.targets) appTarget.deactivate()
+  document.body.removeChild(app.triggerDownload.el)
+  document.documentElement.removeAttribute('data-editable')
+  document.execCommand('defaultParagraphSeparator', false, 'div')
+}
 function elementize (str = '') {
   const el = document.createElement('div')
   el.innerHTML = str
